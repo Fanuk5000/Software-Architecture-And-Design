6e4471e92549dcb3ef541a7e7fb00284ea576d49
illya_mak <illyamak24@gmail.com>
2026-05-10 21:01:19 +0300

    Change encapsulation and add get_all_by

--- Lab4/DataAccess/repository.py
+++ Lab4/DataAccess/repository.py
@@ -12,26 +12,31 @@ class GenericRepository(AbstractRepository[T], Generic[T]):
     def __init__(
         self, session: sqlalchemy.ext.asyncio.AsyncSession, model: Type[T]
     ) -> None:
-        self._session = session
-        self._model = model
+        self.__session = session
+        self.__model = model
 
     async def add(self, entity: T) -> None:
-        self._session.add(entity)
+        self.__session.add(entity)
 
     async def get_by_id(self, id: int) -> Optional[T]:
-        return await self._session.get(self._model, id)
+        return await self.__session.get(self.__model, id)
 
     async def get_all(self) -> list[T]:
-        result = await self._session.execute(select(self._model))
+        result = await self.__session.execute(select(self.__model))
         return list(result.scalars().all())
 
     async def get_one_by(self, **filters) -> Optional[T]:
-        statement = select(self._model).filter_by(**filters)
-        result = await self._session.execute(statement)
+        statement = select(self.__model).filter_by(**filters)
+        result = await self.__session.execute(statement)
         return result.scalars().first()
 
+    async def get_all_by(self, **filters) -> list[T] | list:
+        statement = select(self.__model).filter_by(**filters)
+        result = await self.__session.execute(statement)
+        return list(result.scalars().all())
+
     async def update(self, entity: T) -> None:
-        await self._session.merge(entity)
+        await self.__session.merge(entity)
 
     async def delete(self, entity: T) -> None:
-        await self._session.delete(entity)
+        await self.__session.delete(entity)
